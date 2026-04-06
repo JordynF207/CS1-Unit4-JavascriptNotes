5509a874edc67765869bb4bdbdf88b7d65098ad9
JordynF207 <Jordyn.friedlander2027@gbwl.org>
2026-04-06 15:02:06 +0000

    started java script

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+// This  is a SINGLE LINE COMMENT (note)
+/* This is a MLTI LINE COMMENT
+Code below is an example JS STATEMENT(instruction/command)
+Every statement ends in a SEMICOLON
+*/
+console.log("Welcome to javascript!");
