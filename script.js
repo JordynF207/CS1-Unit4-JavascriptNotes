8aa194ab0eefb6067aa45bfdd538ca3eeaba8e64
JordynF207 <Jordyn.friedlander2027@gbwl.org>
2026-04-13 14:58:29 +0000

    studied varibales today

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ Code below is an example JS STATEMENT(instruction/command)
 Every statement ends in a SEMICOLON
 */
 console.log("Welcome to javascript!");
+
+// VARIABLES are named storage containers for data
+
+// DECLARE a new varibale (make space)
+let username;
+
+// ASSIGN a value to an exsiting variable 
+username = "jordyn.f";
+
+// DECLARE + ASSIGN a new varible in one line
+let pokemon = "pikachu";
+
+// See the value of a varibale 
+console.log(username);
+console.log(pokemon);
+
+// Create a varible that holds a NUMBER
+let luckyNum = 17; // Numbers don't need quotes!
+console.log(luckyNum);
+
+// Change the value stored in a variable
+luckyNum = 19;
+console.log(luckyNum);
